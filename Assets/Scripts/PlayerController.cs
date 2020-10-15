a8443beebdeb53415a2989cc1697dd80686fbecc
AFR3\roe4 <alvinroetube@gmail.com>
2020-10-15 11:28:25 -0400

    End of video 15

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public SpriteRenderer render;
     public int speed = 2;
 
     void Start()
@@ -14,7 +16,17 @@ public class PlayerController : MonoBehaviour
     {
         float hInput = Input.GetAxis("Horizontal");        
         rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
-        
+                
+        //Check to see if we are moving right
+        if(hInput > 0)
+        {
+            render.flipX = false;
+        }
+        //Check to see if we are moving left
+        else if(hInput < 0)
+        {
+            render.flipX = true;
+        }
     }
 
 }
