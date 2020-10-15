77c59eea8235a00c9baf5dac236b53afe455e856
AFR3\roe4 <alvinroetube@gmail.com>
2020-10-15 13:25:12 -0400

    End of Video 23

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,24 +22,51 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        float hInput = Input.GetAxis("Horizontal");
-        float vSpeed = Input.GetButtonDown("Jump") ? jumpPower : rb.velocity.y;
-                
-        if(hInput > 0) 
-            render.flipX = false;
-        else if(hInput < 0)
-            render.flipX = true;
-
-        rb.velocity = new Vector2(hInput * speed, vSpeed);
-
-        if (rb.velocity.y > .5)
-            anim.Play("Jump");
-        else if (rb.velocity.y < -.5)
-            anim.Play("Fall");
-        else if (Mathf.Abs(rb.velocity.x) > 0)
-            anim.Play("Run");
-        else
-            anim.Play("Idle");
+        RunFSM();
     }
 
+    #region Finite State Machine
+    void RunFSM()
+    {
+        if (state == PlayerStates.Idle)
+            IdleState();
+        else if (state == PlayerStates.Run)
+            RunState();
+        else if (state == PlayerStates.Jump)
+            JumpState();
+        else if (state == PlayerStates.Fall)
+            FallState();
+    }
+    void IdleState()
+    {
+        anim.Play("Idle");
+
+        float hInput = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(hInput) > Mathf.Epsilon)
+            state = PlayerStates.Run;
+    }
+    void RunState()
+    {
+        anim.Play("Run");
+
+        float hInput = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(hInput) < Mathf.Epsilon)
+            state = PlayerStates.Idle;
+
+        if (hInput > 0)
+            render.flipX = false;
+        else if (hInput < 0)
+            render.flipX = true;
+
+        rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+    }
+    void JumpState()
+    {
+
+    }
+    void FallState()
+    {
+
+    }
+    #endregion
 }
