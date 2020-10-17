be3ca454ec0f90f75df6bbfd44c524f516a6c981
AFR3\roe4 <alvinroetube@gmail.com>
2020-10-17 12:27:46 -0400

    End of Video 50

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,19 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] float knockBackForce;
 
+    //Finite State Machine Variables
+    AIStates state = AIStates.Patrol;
+
+    //Patrol State Variables
+    Vector3 startingPosition;
+    [SerializeField] Vector3 leftPatrolPoint;
+    [SerializeField] Vector3 rightPatrolPoint;
+
+    void Start()
+    {
+        startingPosition = transform.position;    
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.layer == (int)Layers.Player)
@@ -14,4 +27,36 @@ public class Enemy : MonoBehaviour
             player.SetHurtState(knockBackForce, transform);
         }
     }
+
+    void Update()
+    {
+        RunAI();    
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(leftPatrolPoint + transform.position, .25f);
+        Gizmos.DrawLine(transform.position, leftPatrolPoint + transform.position);
+        Gizmos.DrawWireSphere(rightPatrolPoint + transform.position, .25f);
+        Gizmos.DrawLine(transform.position, rightPatrolPoint + transform.position);
+    }
+
+
+    void RunAI()
+    {
+        switch(state)
+        {
+            case AIStates.Patrol:
+                PatrolState();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void PatrolState()
+    {
+
+    }
+
+
 }
--- Assets/Scripts/Enumerators.cs
+++ Assets/Scripts/Enumerators.cs
@@ -1,2 +1,3 @@
 ﻿public enum PlayerStates {Idle, Run, Jump, Fall, GroundAttack, AirAttack, Hurt}
 public enum Layers {Ground = 8, Enemy, Player}
+public enum AIStates {Patrol}
