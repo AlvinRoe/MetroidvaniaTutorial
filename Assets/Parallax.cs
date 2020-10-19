250cd8b013e970b0929288622019ba523f1050c8
AFR3\roe4 <alvinroetube@gmail.com>
2020-10-19 09:06:24 -0400

    End of Video 61

---
+++ Assets/Parallax.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Transform cameraTransform;
+    Vector3 lastCameraPosition;
+
+    private void Start()
+    {
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 changeInPosition = cameraTransform.position - lastCameraPosition;
+        transform.position = transform.position + changeInPosition;
+        lastCameraPosition = cameraTransform.position;
+    }
+}
