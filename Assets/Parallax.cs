56d1e920e2544c9c819a26f5e19b13b04d90f1d5
AFR3\roe4 <alvinroetube@gmail.com>
2020-10-19 13:38:25 -0400

    End of Video 63

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    public Transform cameraTransform;
-    Vector3 lastCameraPosition;
     [SerializeField] Vector2 parallaxEffectAmount = new Vector2(1f, 1f);
+    Transform cameraTransform;
+    Vector3 lastCameraPosition;
 
     private void Start()
     {
         lastCameraPosition = cameraTransform.position;
+        cameraTransform = Camera.main.transform;
     }
 
     private void LateUpdate()
